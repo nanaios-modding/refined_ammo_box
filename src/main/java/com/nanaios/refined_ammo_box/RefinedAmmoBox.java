fc44b081fd0d7853edfc9f82593a260c69b94556
nanaios <game.ryd.131@gmail.com>
2026-01-20 19:16:52 +0900

    init project

---
+++ src/main/java/com/nanaios/refined_ammo_box/RefinedAmmoBox.java
@@ -0,0 +1,12 @@
+package com.nanaios.refined_ammo_box;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RefinedAmmoBox.MODID)
+public class RefinedAmmoBox {
+    public static final String MODID = "refined_ammo_box";
+
+    public RefinedAmmoBox() {
+
+    }
+}
